8ac90c817fff31fc546636eaa86737003f415d11
lariddiniz <133662692+lariddiniz@users.noreply.github.com>
2024-05-22 15:44:02 -0300

    Initial commit

---
+++ lista 4.cs
@@ -0,0 +1,85 @@
+﻿/*using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Informe um número:");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Tabuada de {numero}:");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+}*/
+
+/*using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Informe um número:");
+
+       
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine($"Tabuada de {numero}:");
+
+        
+        for (int i = 1; i <= 10; i++)
+        {
+            
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+}*/
+/*using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Executa um laço 5 vezes para solicitar 5 números ao usuário
+        for (int i = 0; i < 5; i++)
+        {
+            // Solicita ao usuário que informe um número
+            Console.WriteLine("Informe um número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            // Verifica se o número é maior que 100 e exibe a mensagem apropriada
+            if (numero > 100)
+            {
+                Console.WriteLine("NÚMERO ESPECIAL");
+            }
+            else
+            {
+                Console.WriteLine("NÚMERO COMUM");
+            }
+        }
+    }
+}*/
+
+/*using System;
+
+class Program
+{
+    static void Main()
+    {
+        string resposta;
+
+        do
+        {
+            Console.WriteLine("Deseja continuar? (s/n)");
+            resposta = Console.ReadLine().ToLower();
+        }
+        while (resposta == "s");
+
+        Console.WriteLine("Programa encerrado.");
+    }
+}
+*/
